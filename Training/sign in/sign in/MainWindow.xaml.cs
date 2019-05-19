6e43de1cf50566681ba4e2867782125a6f8ff559
Mahdi Kariman <mp.kariman@gmail.com>
2019-05-19 07:21:24 -0700

    comeback

---
+++ Training/sign in/sign in/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sign_in
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (usernametxt.Text == "ali" && passwordtxt.Password == "1234" && RBmanager.IsChecked == true)
+            {
+                MessageBox.Show("شما با موفقیت وارد شدید", "Sign in", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                manager_page mp1 = new manager_page();
+
+                this.Close();
+                mp1.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("ERROR !", "Sign in", MessageBoxButton.OK, MessageBoxImage.Error);
+                usernametxt.Text = "";
+                passwordtxt.Password = "";
+
+            }
+
+
+        }
+    }
+}
---
+++ Training/sign in/sign in/manager page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sign_in
+{
+    /// <summary>
+    /// Interaction logic for manager_page.xaml
+    /// </summary>
+    public partial class manager_page : Window
+    {
+        public manager_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
