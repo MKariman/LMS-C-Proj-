d891f2c275ff33d7d92010c402d5dbbfe4eb87f6
Mahdi Kariman <mp.kariman@gmail.com>
2019-05-24 09:58:27 -0700

    final

---
+++ Training/Project/WPF(LMS)/WPF(LMS)/BackEnd/Mg_Models.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_LMS_.BackEnd
+{
+    public class Mg_Models
+    {
+        public int Mg_ModelsId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int MeliCode { get; set; }
+        public int OrgCode { get; set; }
+        public int Phone { get; set; }
+    }
+}
