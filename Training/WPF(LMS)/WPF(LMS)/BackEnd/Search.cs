e159ef1af3114ed066a702b3546e07b159ea07b3
Mahdi Kariman <mp.kariman@gmail.com>
2019-06-18 12:01:28 -0700

    ye class be esme search zadam ke toosh 4 ta tabe has. kole karato ba hamin class bokon

---
+++ Training/WPF(LMS)/WPF(LMS)/BackEnd/Search.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_LMS_.BackEnd
+{
+    public class Search
+    {
+
+        public List<St_Models> STsearch(string UCS)
+        {
+            using (var Db = new DB_Proj())
+            {
+                var login = (List<St_Models>)Db.St_InfM.Where(i => i.UniCode.ToString().StartsWith(UCS));
+                return login;
+            }
+        }
+        public Mg_Models MgSearch(int usern)
+        {
+            using (var Db = new DB_Proj())
+            {
+                var login = Db.Mg_InfM.Where
+                (i => i.OrgCode == usern).FirstOrDefault();
+                return login;
+            }
+
+        }
+        public St_Models STSearch(int usern)
+        {
+            using (var Db = new DB_Proj())
+            {
+                var login = Db.St_InfM.Where
+                (i => i.UniCode == usern).FirstOrDefault();
+                return login;
+            }
+        }
+        public Tch_Models TchSearch(int usern)
+        {
+            using (var Db = new DB_Proj())
+            {
+                var login = Db.Tch_InfM.Where
+                (i => i.OrgCode == usern).FirstOrDefault();
+                return login;
+            }
+        }
+    }
+}
