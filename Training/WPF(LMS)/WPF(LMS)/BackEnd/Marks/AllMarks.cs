20880693286b16c48d0874b6e336a96b8142624c
Mahdi Kariman <mp.kariman@gmail.com>
2019-06-30 09:59:53 -0700

    edit database

--- Training/WPF(LMS)/WPF(LMS)/BackEnd/Marks/AllMarks.cs
+++ Training/WPF(LMS)/WPF(LMS)/BackEnd/Marks/AllMarks.cs
@@ -10,10 +10,10 @@ namespace WPF_LMS_.BackEnd.Marks
     {
         public int AllMarksId { get; set; }
         public string Major { get; set; }
-        public int? Riazi1 { get; set; }
-        public int? Moadelat { get; set; }
-        public int? Adabiat { get; set; }
-        public int? Andishe { get; set; }
+        public int Riazi1 { get; set; }
+        public int Moadelat { get; set; }
+        public int Adabiat { get; set; }
+        public int Andishe { get; set; }
 
         public int St_ModelsId { get; set; }
 
