c62b22b40aec28cbf38cde203fb484971fe74356
Mahdi Kariman <mp.kariman@gmail.com>
2019-07-04 09:38:02 -0700

    Edit Marks
    edite mark ham zadam ...

--- Training/WPF(LMS)/WPF(LMS)/BackEnd/Edit.cs
+++ Training/WPF(LMS)/WPF(LMS)/BackEnd/Edit.cs
@@ -78,6 +78,123 @@ namespace WPF_LMS_.BackEnd
             }
         }
 
+       // public string ChangeMark(int UCode,string Maj,List)
+
+
+        public void CMComp(int UCode, List<Marks.CompM> MarkList)
+        {
+            using(DB_Proj db=new DB_Proj())
+            {
+                int STID =(from item in db.St_InfM where item.UniCode == UCode select item.St_ModelsId).FirstOrDefault();
+                var ST = db.CompD.Where(i => i.St_ModelsId == STID).FirstOrDefault();
+                ST.Adabiat = MarkList[0].Adabiat;
+                ST.Andishe = MarkList[0].Andishe;
+                ST.CDatabase = MarkList[0].CDatabase;
+                ST.CGosaste = MarkList[0].CGosaste;
+                ST.CInteligence = MarkList[0].CInteligence;
+                ST.CManteqi = MarkList[0].CManteqi;
+                ST.CProgramming = MarkList[0].CProgramming;
+                ST.Moadelat = MarkList[0].Moadelat;
+                ST.Riazi1 = MarkList[0].Riazi1;
+                db.SaveChanges();
+
+            }
+        }
+        public void CMOmran(int UCode, List<Marks.OmranM> MarkList)
+        {
+            using (DB_Proj db = new DB_Proj())
+            {
+                int STID = (from item in db.St_InfM where item.UniCode == UCode select item.St_ModelsId).FirstOrDefault();
+                var ST = db.OmranD.Where(i => i.St_ModelsId == STID).FirstOrDefault();
+                ST.Adabiat = MarkList[0].Adabiat;
+                ST.Andishe = MarkList[0].Andishe;
+                ST.Moadelat = MarkList[0].Moadelat;
+                ST.OBotons = MarkList[0].OBotons;
+                ST.ODinamik = MarkList[0].ODinamik;
+                ST.OMasaleh = MarkList[0].OMasaleh;
+                ST.ONaqshe = MarkList[0].ONaqshe;
+                ST.OStatik = MarkList[0].OStatik;
+                ST.Riazi1 = MarkList[0].Riazi1;
+                db.SaveChanges();
+
+            }
+        }
+        public void CMBarq(int UCode, List<Marks.BarqM> MarkList)
+        {
+            using (DB_Proj db = new DB_Proj())
+            {
+                int STID = (from item in db.St_InfM where item.UniCode == UCode select item.St_ModelsId).FirstOrDefault();
+                var ST = db.BarqD.Where(i => i.St_ModelsId == STID).FirstOrDefault();
+                ST.Adabiat = MarkList[0].Adabiat;
+                ST.Andishe = MarkList[0].Andishe;
+                ST.Moadelat = MarkList[0].Moadelat;
+                ST.BCmos = MarkList[0].BCmos;
+                ST.BElectro = MarkList[0].BElectro;
+                ST.BMadar = MarkList[0].BMadar;
+                ST.BQodrat = MarkList[0].BQodrat;
+                ST.BVLSI = MarkList[0].BVLSI;
+                ST.Riazi1 = MarkList[0].Riazi1;
+                db.SaveChanges();
+
+            }
+        }
+        public void CMMech(int UCode, List<Marks.MechM> MarkList)
+        {
+            using (DB_Proj db = new DB_Proj())
+            {
+                int STID = (from item in db.St_InfM where item.UniCode == UCode select item.St_ModelsId).FirstOrDefault();
+                var ST = db.MechD.Where(i => i.St_ModelsId == STID).FirstOrDefault();
+                ST.Adabiat = MarkList[0].Adabiat;
+                ST.Andishe = MarkList[0].Andishe;
+                ST.Moadelat = MarkList[0].Moadelat;
+                ST.MControl = MarkList[0].MControl;
+                ST.MFizik = MarkList[0].MFizik;
+                ST.MMashin = MarkList[0].MMashin;
+                ST.MSayalat = MarkList[0].MSayalat;
+                ST.MTermo = MarkList[0].MTermo;
+                ST.Riazi1 = MarkList[0].Riazi1;
+                db.SaveChanges();
+
+            }
+        }
+        public void CMShimi(int UCode, List<Marks.ShimiM> MarkList)
+        {
+            using (DB_Proj db = new DB_Proj())
+            {
+                int STID = (from item in db.St_InfM where item.UniCode == UCode select item.St_ModelsId).FirstOrDefault();
+                var ST = db.ShimiD.Where(i => i.St_ModelsId == STID).FirstOrDefault();
+                ST.Adabiat = MarkList[0].Adabiat;
+                ST.Andishe = MarkList[0].Andishe;
+                ST.Moadelat = MarkList[0].Moadelat;
+                ST.SHGaz = MarkList[0].SHGaz;
+                ST.SHHararat = MarkList[0].SHHararat;
+                ST.SHJerm = MarkList[0].SHJerm;
+                ST.SHMavad = MarkList[0].SHMavad ;
+                ST.SHSinema = MarkList[0].SHSinema;
+                ST.Riazi1 = MarkList[0].Riazi1;
+                db.SaveChanges();
+
+            }
+        }
+        public void CMMemari(int UCode, List<Marks.MemM> MarkList)
+        {
+            using (DB_Proj db = new DB_Proj())
+            {
+                int STID = (from item in db.St_InfM where item.UniCode == UCode select item.St_ModelsId).FirstOrDefault();
+                var ST = db.MemD.Where(i => i.St_ModelsId == STID).FirstOrDefault();
+                ST.Adabiat = MarkList[0].Adabiat;
+                ST.Andishe = MarkList[0].Andishe;
+                ST.Moadelat = MarkList[0].Moadelat;
+                ST.MeDekor = MarkList[0].MeDekor;
+                ST.MeFazas = MarkList[0].MeFazas;
+                ST.MeNama = MarkList[0].MeNama;
+                ST.MeNaqshe = MarkList[0].MeNaqshe;
+                ST.MeTarahi = MarkList[0].MeTarahi;
+                ST.Riazi1 = MarkList[0].Riazi1;
+                db.SaveChanges();
+            }
+        }
+
         public string ChangePass(int Code,string OPass,string NPass,string Who)
         {
             using (DB_Proj db =new DB_Proj())
@@ -128,6 +245,7 @@ namespace WPF_LMS_.BackEnd
 
             }
         }
+
 
 
 
