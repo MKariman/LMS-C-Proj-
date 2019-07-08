2ad298c39c0d88ad3925a84b571ff35c6abc3856
Mahdi Kariman <mp.kariman@gmail.com>
2019-07-08 13:22:01 -0700

    NullList Of Lessons

--- Training/WPF(LMS)/WPF(LMS)/BackEnd/EnteKhabVahed.cs
+++ Training/WPF(LMS)/WPF(LMS)/BackEnd/EnteKhabVahed.cs
@@ -16,15 +16,75 @@ namespace WPF_LMS_.BackEnd
             {
                 var StInfo= db.St_InfM.Where(a => a.UniCode == UC).FirstOrDefault();
 
-                  var ab = StInfo.GetType().GetProperties().Where(a => a.GetValue(StInfo) == null).Select(b=>b.Name).ToList();
-                
-                for (int i=0;i<ab.Count();i++)
+                if (Maj == "Computer")
                 {
-                    NuLLess.Add(ab[i]);
+                    var ComRow = db.CompD.Where(i => i.St_ModelsId == StInfo.St_ModelsId).FirstOrDefault();
+                    var LLN = ComRow.GetType().GetProperties().Where(a => a.GetValue(ComRow) == null).Select(b => b.Name).ToList();
+                    for (int i = 0; i < LLN.Count(); i++)
+                    {
+                        NuLLess.Add(LLN[i]);
+                    }
 
+                    return NuLLess;
+                }
+                if (Maj == "Barq")
+                {
+                    var ComRow = db.BarqD.Where(i => i.St_ModelsId == StInfo.St_ModelsId).FirstOrDefault();
+                    var LLN = ComRow.GetType().GetProperties().Where(a => a.GetValue(ComRow) == null).Select(b => b.Name).ToList();
+                    for (int i = 0; i < LLN.Count(); i++)
+                    {
+                        NuLLess.Add(LLN[i]);
+                    }
+
+                    return NuLLess;
+                }
+                if (Maj == "Mechanic")
+                {
+                    var ComRow = db.MechD.Where(i => i.St_ModelsId == StInfo.St_ModelsId).FirstOrDefault();
+                    var LLN = ComRow.GetType().GetProperties().Where(a => a.GetValue(ComRow) == null).Select(b => b.Name).ToList();
+                    for (int i = 0; i < LLN.Count(); i++)
+                    {
+                        NuLLess.Add(LLN[i]);
+                    }
+
+                    return NuLLess;
+                }
+                if (Maj == "Omran")
+                {
+                    var ComRow = db.OmranD.Where(i => i.St_ModelsId == StInfo.St_ModelsId).FirstOrDefault();
+                    var LLN = ComRow.GetType().GetProperties().Where(a => a.GetValue(ComRow) == null).Select(b => b.Name).ToList();
+                    for (int i = 0; i < LLN.Count(); i++)
+                    {
+                        NuLLess.Add(LLN[i]);
+                    }
+
+                    return NuLLess;
+                }
+                if (Maj == "Shimi")
+                {
+                    var ComRow = db.ShimiD.Where(i => i.St_ModelsId == StInfo.St_ModelsId).FirstOrDefault();
+                    var LLN = ComRow.GetType().GetProperties().Where(a => a.GetValue(ComRow) == null).Select(b => b.Name).ToList();
+                    for (int i = 0; i < LLN.Count(); i++)
+                    {
+                        NuLLess.Add(LLN[i]);
+                    }
+
+                    return NuLLess;
+                }
+                if (Maj == "Memari")
+                {
+                    var ComRow = db.MemD.Where(i => i.St_ModelsId == StInfo.St_ModelsId).FirstOrDefault();
+                    var LLN = ComRow.GetType().GetProperties().Where(a => a.GetValue(ComRow) == null).Select(b => b.Name).ToList();
+                    for (int i = 0; i < LLN.Count(); i++)
+                    {
+                        NuLLess.Add(LLN[i]);
+                    }
+
+                    return NuLLess;
                 }
 
                 return NuLLess;
+
             }
         }
     }
